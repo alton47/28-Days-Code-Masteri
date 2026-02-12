860672659d3bb3f03d2280ffb62df6973a74a717
Allan <altonallan47@gmail.com>
2026-02-12 22:09:25 +0300

    Update app.js

--- 09/Subscription/app.js
+++ 09/Subscription/app.js
@@ -1,1 +1,14 @@
-console.log("Matkaoo");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Mambooooo!!");
+});
+
+app.listen(port, () => {
+  console.log("Mambo kengeeeee");
+});
+
+export default app;
